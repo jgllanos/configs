c1c6aa6f07d37cf1dccc86b706387104ce0a46ef
Juan G. Llanos <git@jgllanos.com>
2019-03-18 12:22:50 -0500

    Update setup to ignore files removed after cleanup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 git clone --bare https://github.com/jgllanos/configs.git $HOME/.dotfiles
+
 function dotfiles {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
+echo "alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.bashrc
+
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
-echo "alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.bashrc
+
+dotfiles update-index --skip-worktree README.md setup.sh LICENSE
 rm $HOME/README.md $HOME/LICENSE $HOME/setup.sh
